b6856118f0ef34f18a21d85d23e578a7c22fe062
Gerard Chin <gerardchin91@gmail.com>
2017-09-14 19:29:02 +1200

    xd

--- blueprints/base/config.tf
+++
@@ -1,9 +0,0 @@
-# [sleepless] main remote state backend
-terraform {
-  backend "s3" {
-    bucket         = "sleepless-iac-state"
-    key            = "base/terraform.tfstate"
-    region         = "ap-southeast-2"
-    dynamodb_table = "SleeplessIacStateLockSvc"
-  }
-}
